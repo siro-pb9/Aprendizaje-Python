6907d6be4733f3f5047dccb9ee645062e255f0bd
Siro.pb9 <ciroviolayt@gmail.com>
2026-02-07 19:36:22 -0300

    Add files via upload

---
+++ Hello Word.py
@@ -0,0 +1,23 @@
+test = "funca"
+print(f"Hola, Mundo! {test}")
+
+# Este es un comentario
+# si quieres escribir un comentario de varias líneas, puedes usar comillas triples
+"""2+2=4
+3+3=6
+4+4=8
+5+5=10
+y así sucesivamente..."""
+
+# También puedes usar comillas simples para escribir un comentario de varias líneas
+'''2+2=4
+3+3=6
+4+4=8
+5+5=10
+y así sucesivamente...'''
+
+''' Por cierto, si estas leyendo esto, te agradezco que te tomes del tiempo de leer mi repositorio de HitHub
+Esto NO lo hago para monetizar o tomar credito de nada, si no para incitar a que mas personas que no saben
+programar, se animen a aprender a programar, y si quieres ayudarme a mejorar el repositorio, puedes hacer un pull request
+o enviarme a mi instagra. nuevamente gracias por leer esto y espero que te haya interesado lo suficiente como para seguir
+leyendo el resto del repositorio, con todos los codigos, Suerte ^_^'''
